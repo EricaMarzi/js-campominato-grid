5b4d051ca28b0e3de848a69338e833e7750e2cd8
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-10 18:28:02 +0100

    grid js

---
+++ script.js
@@ -0,0 +1,34 @@
+const grid = document.getElementById("grid")
+const form = document.querySelector("form")
+
+
+
+const rows = 10 //da cambiare in seguito per la select
+const cols = 10 //da cambiare in seguito per la select
+const totalCells = rows * cols;
+
+const createNewCell = (num) => {
+    const newCell = document.createElement("div")
+    //newCell.classList.add("cell")
+    newCell.classList.add("cell")
+    newCell.innerText = num
+    return newCell
+}
+
+
+form.addEventListener ("submit", function(event) {
+    event.preventDefault()
+    
+ 
+    for(let i = 1; i <= totalCells; i++ ) {
+        const cell = createNewCell(i)
+        
+        cell.addEventListener ("click", () => {
+            cell.classList.toggle("bg-cell")
+        })
+            
+            
+        grid.appendChild(cell)
+    }
+})
+
